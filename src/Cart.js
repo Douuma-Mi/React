b39504a9c47303a6aae591063b534e742f9dead1
Douuma_Mi <ademmathloouthi27705220@gmail.com>
2023-09-16 12:36:52 +0100

    first commit

---
+++ src/Cart.js
@@ -0,0 +1,36 @@
+import React from 'react'
+import Card from 'react-bootstrap/Card';
+
+function Cart() {
+  return (
+    <div>
+        <>
+      {[
+        'Primary',
+        'Secondary',
+        'Success',
+      ].map((variant) => (
+        <Card
+          bg={variant.toLowerCase()}
+          key={variant}
+          text={variant.toLowerCase() === 'light' ? 'dark' : 'white'}
+          style={{ width: '18rem' }}
+          className="mb-2"
+        >
+          <Card.Header>Header</Card.Header>
+          <Card.Body>
+            <Card.Title>{variant} Card Title </Card.Title>
+            <Card.Text>
+              Some quick example text to build on the card title and make up the
+              bulk of the card's content.
+            </Card.Text>
+          </Card.Body>
+        </Card>
+      ))}
+    </>
+
+    </div>
+  )
+}
+
+export default Cart
